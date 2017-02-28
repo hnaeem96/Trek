d62c148161d8474516084d20fb2d745c6608cd92
hnae6443 <hnae6443@gmail.com>
2017-02-27 19:12:01 -0500

    Added waypoints function to show sticky navigation bar when scrolling down

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,11 @@
 $(document).ready(function () {
+
+    $('.js-nav').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky-navigation');
+        }
+        else {
+            $('nav').removeClass('sticky-navigation');
+        }
+    });    
 });
